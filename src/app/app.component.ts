c710a7283c775212ece7d1b0347e8ec21c65ee31
shirousaberx <michaelalexander6666@outlook.com>
2024-07-13 22:52:09 +0700

    section 3.1 on youtube

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WishItem } from '../shared/modules/wishItem';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,39 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  items : WishItem[] = [
+    new WishItem('To learn Angular'),
+    new WishItem('Get Coffee', true),
+    new WishItem('Find grass that cuts itself')
+  ]
+
+  listFilter = '0'
+
+  newWishText = ''
+
   title = 'wishlist';
+
+  get visibleItems() : WishItem[] {
+    let value = this.listFilter
+
+    if (value === '0') {
+      return this.items;
+    } else if (value ==='1') {
+      return this.items.filter(item => !item.isCompleted)
+    } else {
+      return this.items.filter(item => item.isCompleted)
+    }
+  }
+
+  addNewWish() {
+    // add wish to items array
+    // clear text box
+    this.items.push(new WishItem(this.newWishText));
+    this.newWishText = '';
+  }
+
+  toogleItem(item : WishItem) {
+    item.isCompleted = !item.isCompleted
+    console.log(item)
+  }
 }
